0ab3820939881ddc41cd838986e290d836b9e703
metador <melwinj7@gmail.com>
2015-05-15 16:26:31 -0700

    First commit
    first commit

---
+++ src/project4/Parser.java
@@ -0,0 +1,11 @@
+package project4;
+
+public class Parser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+DomParserExample dom =  new DomParserExample();
+dom.runExample();
+	}
+
+}
