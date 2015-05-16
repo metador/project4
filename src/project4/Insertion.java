4ccb88032ac60dfde79f60de2d9e2e483cf41ee5
mukhtar89 <muhammad.28.1989@gmail.com>
2015-05-15 18:36:41 -0700

    insert update

--- src/project4/Insertion.java
+++ src/project4/Insertion.java
@@ -7,18 +7,19 @@ import java.sql.*;
 public class Insertion 
 {
 	private static ArrayList<DBLP> books = new ArrayList<DBLP> ();
-	private static DBLP book = new DBLP();
+	private DBLP book = new DBLP();
 	public static Connection connection;
-	public static PreparedStatement idSearch;
-	public static String idQuery;
-	public static ResultSet idResult;
+	public PreparedStatement idSearch, loadTable;
+	public String idQuery;
+	public ResultSet idResult;
 	public int ID = -1;
+	private HashMap<DBLP, Integer> returnID = new HashMap<DBLP, Integer>();
 	private HashMap<String,Integer> genre = new HashMap<String,Integer>();
 	private HashMap<String,Integer> people = new HashMap<String,Integer>();
 	private HashMap<String,Integer> title = new HashMap<String,Integer>();
 	private HashMap<String,Integer> publisher = new HashMap<String,Integer>();
 	
-	public static void Insertion(ArrayList<DBLP> bks, Connection connect)
+	public Insertion(ArrayList<DBLP> bks, Connection connect)
 	{
 		books = bks;
 		connection = connect;
@@ -35,12 +36,7 @@ public class Insertion
 				{
 					idQuery = "insert into tbl_genres set genre_name = '" + name + "';";
 					idSearch = (PreparedStatement) connection.prepareStatement(idQuery);
-					idSearch.executeUpdate();
-					idQuery = "select id from tbl_genres where genre_name = '" + name + "';";
-					idSearch = (PreparedStatement) connection.prepareStatement(idQuery);
-					idResult = idSearch.executeQuery();
-					idResult.next();
-					ID = Integer.parseInt(idResult.getString("id"));
+					ID = idSearch.executeUpdate(idQuery, Statement.RETURN_GENERATED_KEYS);
 					genre.put(name, ID);
 				}
 				break;
@@ -56,17 +52,28 @@ public class Insertion
 		return ID;
 	}
 	
-	public void insert() 
+	public void insert() throws SQLException 
 	{
 		int iter = 0;
-		String query = "insert into tbl_dblp_document ('title', 'start_page', 'end_page', 'year', "
+		String insertHeadDoc = "insert into tbl_dblp_document ('title', 'start_page', 'end_page', 'year', "
 				+ "'volume', 'number' , 'url', 'ee', 'cdrom', 'cite', 'crossref', 'isbn', 'series',"
-				+ " 'editor_id', 'booktitle_id', 'genre_id', 'publisher_id') values (";
-		while (books.isEmpty())
+				+ " 'editor_id', 'booktitle_id', 'genre_id', 'publisher_id') values ";
+		String insertHeadAuthor = "insert into tbl_author_document_mapping ('doc_id', 'author_id')";
+		String query = insertHeadDoc;
+		while (iter != books.size())
 		{
-			book = books.remove(iter);
+			book = books.get(iter);
+			query = " (";
 			query += "'" + book.getTitle() + "', '" + book.getStart_page() + "', '" + book.getEnd_page() + "',";
-			query += " '" + book.getYear() + "', '" + book.getVolume() + ;
+			query += " '" + book.getYear() + "', '" + book.getVolume() + "', '" + book.getNumber() + "',";
+			query += " '" + book.getUrl() + "', '" + book.getEe() + "', '" + book.getCdrom() + "',";
+			query += " '" + book.getCite() + "', '" + book.getCrossref() + "', '" + book.getIsbn() + "',";
+			query += " '" + book.getSeries() + "', '" + searchID(book.getEditor(),"people") + "', '" + searchID(book.getBooktitle(),"title") + "',";
+			query += " '" + searchID(book.getGenre(),"genre") + "', '" + searchID(book.getPublisher(),"publisher") + "')";
+			
+			loadTable = (PreparedStatement) connection.prepareStatement(query);
+			ID = loadTable.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+			returnID.put(book, ID);
 		}
 	}
 }
