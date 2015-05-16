e63d2201209e23b33693800ed01ab97f56ffe99a
metador <melwinj7@gmail.com>
2015-05-15 17:15:36 -0700

    third commit
    maps made

--- src/project4/DBLP.java
+++ src/project4/DBLP.java
@@ -26,14 +26,59 @@ public class DBLP {
 	private ArrayList<String> authors=new ArrayList<String>();
 	private String booktitle="";
 	private String genre="";
-	private String publisher="";
+	  String publisher="";
 	public DBLP(){
 		
 	}
 	
+public DBLP(
+		  String title,
+		  int start_page,
+		  int end_page,
+		  int year,
+		  int volume,
+		  int number,
+		  String url ,
+		  String ee ,
+		  String cdrom ,
+		  String cite ,
+		  String crossref ,
+		  String isbn ,
+		  String series ,
+		  String editor ,
+		  ArrayList<String> authors,
+		  String booktitle ,
+		  String genre 
+		){
+	  this. title=title;
+	  this. start_page=start_page;
+	  this. end_page=end_page;
+	  this. year=year;
+	  this. volume=volume;
+	  this. number=number;
+	  this. url =url;
+	  this. ee =ee;
+	  this. cdrom =cdrom;
+	  this. cite =cite;
+	  this. crossref =crossref;
+	  this. isbn =isbn;
+	  this. series =series;
+	  this. editor =editor;
+	  this.authors=authors;
+	  this. booktitle =booktitle;
+	  this. genre =genre;
+		
+	}
 	
+	public String getTitle() {
+	return title;
+	}
+
+	public void setTitle(String title) {
+	this.title = title;
+	}
 	public int getStart_page() {
-		return start_page;
+		return start_page ;
 	}
 
 	public void setStart_page(int start_page) {
@@ -48,24 +93,129 @@ public class DBLP {
 		this.end_page = end_page;
 	}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
+	
 
 
 	public int getYear() {
 		return year;
 	}
 
-	public void setType(int year) {
+	public void setYear(int year) {
 		this.year = year;
 	}	
+	public int getVoulume() {
+		return volume;
+	}
+
+	public void setVoulme(int volume) {
+		this.volume = year;
+	}
 	
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getEe() {
+		return ee;
+	}
+
+	public void setEe(String Ee) {
+		this.ee = Ee;
+	}
+	
+	public String getCdrom() {
+		return cdrom;
+	}
+
+	public void setCdrom(String Cdrom) {
+		this.cdrom = Cdrom;
+	}
+	
+	public String getCite() {
+		return cite;
+	}
+
+	public void setCite(String Cite) {
+		this.cite = Cite;
+	}
+	
+	public String getCrossref() {
+		return crossref;
+	}
+
+	public void setCrossref(String Crossref) {
+		this.crossref = Crossref;
+	}
+	
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String Isbn) {
+		this.isbn = Isbn;
+	}
+	
+	public String getSeries() {
+		return series;
+	}
+
+	public void setSeries(String Series) {
+		this.series = Series;
+	}
+	
+	public String getEditor() {
+		return editor;
+	}
+
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+	
+	public String getBooktitle() {
+		return booktitle;
+	}
+
+	public void setBooktitle(String Booktitle) {
+		this.booktitle = Booktitle;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String Publisher) {
+		this.publisher = Publisher;
+	}
+	 
+	
+	public ArrayList<String> getAuthor() {
+		return authors;
+	}
+
+	public void setPublisher(ArrayList<String>  authors) {
+		this.authors = authors;
+	}
+	 	
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("Employee Details - ");
